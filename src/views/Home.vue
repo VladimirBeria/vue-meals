b831c6aa4e41a59fd1f8bc6b77eb556355f60ef4
Volodymyr Dekhtyaruk <volodymyr.dekhtyaruk@sporthub.tech>
2024-07-12 01:29:39 +0300

    feature: request and response from API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,32 @@
 <template>
-  <h1 class="bg-blue-100">{{ meals }}</h1>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+    >
+    <div class="flex gap-2 mt-2 justify-center">
+      <router-link
+        v-for="letter of letters"
+        :to="{ name: 'byLetter', params: { letter } }"
+        :key="letter"
+        :letter="letter"
+        class="btn"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { computed } from 'vue';
-import store from '../store/index.js';
+import { onMounted } from 'vue';
+import axiosClient from '../axiosClient.js';
 
-const meals = computed(() => store.state.meals);
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+
+onMounted(async () => {
+  const response = await axiosClient.get('/list.php?i=list')
+  console.log(response.data);
+});
 </script>
---
+++ src/views/MealList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+    Meal List
+  </div>
+</template>
+<script setup>
+
+</script>
+
+
+<style scoped>
+
+</style>
